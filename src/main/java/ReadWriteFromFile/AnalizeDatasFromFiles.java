7433d87a7fbbd3cd75eb91e104d8682c5b7675ee
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-07-30 11:17:29 +0200

    Get line's from file

--- src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
+++ src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
@@ -1,6 +1,9 @@
 package ReadWriteFromFile;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AnalizeDatasFromFiles {
 
@@ -12,8 +15,22 @@ public class AnalizeDatasFromFiles {
         File directory = new File(filesDirectory);
         File[] filesList = directory.listFiles();
 
-        for (File file : filesList){
-            System.out.println(file);
+        for (File file : filesList) {
+            if (file.isFile()) {
+                String fileName = file.getName();
+
+                //            Read lines from files
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String currentLine;
+
+                    while ((currentLine = bufferedReader.readLine()) != null) {
+                        System.out.println(currentLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
         }
 
     }
