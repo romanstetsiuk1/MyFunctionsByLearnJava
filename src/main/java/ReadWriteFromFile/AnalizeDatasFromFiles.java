8999353d6f91d9916d00563af4b9d6efba743ef7
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-07-30 11:05:33 +0200

    Get files list from directory

--- src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
+++ src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
@@ -1,4 +1,21 @@
 package ReadWriteFromFile;
 
+import java.io.File;
+
 public class AnalizeDatasFromFiles {
+
+    public static void main(String[] args) {
+
+        String filesDirectory = "C:\\Users\\roman.stetsiuk\\Documents\\VK\\XTB_raports";
+
+        //        Get list of file in PC directory
+        File directory = new File(filesDirectory);
+        File[] filesList = directory.listFiles();
+
+        for (File file : filesList){
+            System.out.println(file);
+        }
+
+    }
+
 }
