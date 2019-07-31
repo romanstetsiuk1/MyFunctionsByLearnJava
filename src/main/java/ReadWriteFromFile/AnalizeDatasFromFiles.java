be35cb965b210a5aca67f0f994c85634e3ad8ad3
romanstetsiuk1 <romansteciuk021@gmail.com>
2019-07-31 13:26:19 +0200

    Add logger

--- src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
+++ src/main/java/ReadWriteFromFile/AnalizeDatasFromFiles.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class AnalizeDatasFromFiles {
 
@@ -15,6 +18,25 @@ public class AnalizeDatasFromFiles {
         //        Get list of file in PC directory
         File directory = new File(filesDirectory);
         File[] filesList = directory.listFiles();
+
+//        Logfile
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("C:\\Users\\roman.stetsiuk\\IdeaProjects\\MyProgramsByLearn\\logs\\LogFile.log");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+            logger.info("Hello");
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         for (File file : filesList) {
             if (file.isFile()) {
